c508b36943fac8f91f6d4ea4dc7c529b7e0e60b8
Eli Metzler-Prieb <elimetzlerprieb@gmail.com>
2017-03-13 18:10:26 -0400

    Rewrite code to better follow OOP guidelines. drawingPad now an object

--- script.js
+++ script.js
@@ -1,38 +1,48 @@
-$(document).ready(function() {
-	var pixel_count = 0;
-	var sketch_pad_size = 500;
+$(document).ready(function(){
+	var drawingPad = new drawingPad();
+	var canvasSize = 600; //Desired dimension of pad in display pixels
+	var defaultPixel = 25; //Desired dimension of pad in "8 bit" style pixels
 
-	if ((pixel_count == 0) || (pixel_count > 100)) {
+	drawingPad.build();
+	drawingPad.draw();
+
+
+function drawingPad(pixel_count=defaultPixel) {
+
+	// Builds drawing pad consisting of pixel_count x pixel_count macro pixels
+	// in a canvasSize x canvasSize space
+	this.build = function(pixel_count=defaultPixel) {
+		// Appends i = pixel_count number of rows to the statically coded HTML table
+		for(var i = 0; i < pixel_count; i++) {
+			$("table").append($("<tr></tr>"));
+		}
+
+		//Appends i = pixel_count table data elements to each row. Each td includes a 'pixel' class div 
+		for(var i = 0; i < pixel_count; i++) {
+			$("tr").append($("<td><div class='pixel'></div></td>"));
+		}
+
+		// Rounds 'pixel' size to nearest integer to avoid partial pixel display issues
+		var size = Math.round(canvasSize/pixel_count)
+
+		// Resizes 'pixel' class division so pixel_count 'pixels' fit into an approximately sketch_pad_size square
+		$(".pixel").height(size);
+		$(".pixel").width(size);
+	}
+
+	// Provides functionality for dynamic changes when mouse moves through a pixel
+	this.draw = function () {
+		// Changes 'pixels' when mouse enters and
+		$(".pixel").hover(function() {
+			$(this).css("background-color", "black");
+		});
+	}
+
+	// Resets function and prompts for a different size grid if desired
+	this.clear = function () { 
 		pixel_count = prompt("How big should the grid be? (In pixels)");
+		this.build;
 	}
+}
 
-	// Appends x = pixel_count number of rows to the statically coded HTML table
-	for(var i = 0; i < pixel_count; i++) {
-		$("table").append($("<tr></tr>"));
-	}
-	// Appends x = pixel_count table data elements to each row. Each td includes a 'pixel' class div 
-	for(var i = 0; i < pixel_count; i++) {
-		$("tr").append($("<td><div class='pixel'></div></td>"));
-	}
-
-	// Rounds 'pixel' size to nearest integer to avoid patial pixel display issues
-	var size = Math.round(sketch_pad_size/pixel_count);
-
-	// Resizes 'pixel' class division so pixel_count 'pixels' fit into an approximately sketch_pad_size square
-	$(".pixel").height(size);
-	$(".pixel").width(size);
-
-	// Changes 'pixels' when mouse enters and leaves
-	$(".pixel").hover(function() {
-		$(this).css("background-color", "black");
-	});
-	/*$(".pixel").mouseleave(function() {
-
-	});
-	*/
-
-
-
-});
-
-
+})
